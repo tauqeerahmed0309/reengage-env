1019fac75775f2b251b328535328e1b84616f92f
Tauqeer Ahmed <tauqeerahmed0305@gmail.com>
2026-04-08 20:16:32 +0530

    Update app.py

--- server/app.py
+++ server/app.py
@@ -1,8 +1,10 @@
 from server import app
-from server import app
 import uvicorn
 import os
 
 def main():
     port = int(os.environ.get("PORT", 7860))
     uvicorn.run(app, host="0.0.0.0", port=port)
+
+if __name__ == "__main__":
+    main()
